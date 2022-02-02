b30563647b7f05cb11bcbfa248a95c0ba6cfe09e
Leonardo Lugo <leolugo5@outlook.com>
2022-02-02 15:51:28 -0500

    Pre finished

--- reviews.js
+++ reviews.js
@@ -1,35 +1,3 @@
-const comments = [
-
-    {
-        "id": "1",
-        "comment": "Our group had a great time at Scuba Hunt. If you are looking to try a new challenge underwater, they add a whole new twist to the underwater world.",
-        "author": "- Bret"
-    },
-    {
-        "id": "2",
-        "comment": "If you're looking for somewhere to treat you like family, this is the place. The crew was amazing and patient. The scavenger hunt and clues were challenging but A LOT of fun!! I would dothis adventure again and again.",
-        "author": "- Tiffany"
-    },
-    {
-        "id": "3",
-        "comment": "I cannot say enough great things about this adventure! Was way better than even my expectations. Highly recommend it if you like puzzles, scavenger hunts, diving lol! Such a great way to improve your buoyancy, navigation, and underwater communication.",
-        "author": "- Caitlin"
-    },
-    {
-        "id": "4",
-        "comment": "Our family of four had an outstanding time competing against each other at SCUBA hunt! Also, its location at Hemingway is awesome as that is a cool spot to eat, drink, and hang out before or after.",
-        "author": "- Allen"
-    },
-    {
-        "id": "5",
-        "comment": "The Scuba hunt team is super professional, safety is their priority, the equipment they are using is brand new and excellent. After the game, you can chill and watch the sunset in one of the most popular beach bars in Cozumel.",
-        "author": "- Bret"
-    }
-
-]
-
-/* comment-selector1 se-btn */
-
 const btn1 = document.getElementById('rev-selector1')
 const btn2 = document.getElementById('rev-selector2')
 const btn3 = document.getElementById('rev-selector3')
